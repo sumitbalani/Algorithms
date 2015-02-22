c2e5923722a84296795cefccb0fa0865bd9225ac
Sumit Balani <sbalani@uwaterloo.ca>
2015-02-21 20:59:59 -0500

    Todo: fix final maximal check logic

---
+++ src/MaximalPoints.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MaximalPoints {
+
+	class point {
+		int x;
+		int y;
+		
+		public boolean isGreater(point p1, point p2){
+			if(p1.x>p2.x && p1.y > p2.y){
+				return true;
+			}
+			return false;
+		}
+	}
+	
+	public static List<point> maximalPoints(List<point> points){
+		if(points.size() == 1){
+			return points;
+		}
+		
+		List<point> left = new ArrayList<point>();
+		for(int i = 0; i<points.size()/2; i++){
+			left.add(points.get(i));
+		}
+		left = maximalPoints(left);
+		
+		List<point> right = new ArrayList<point>();
+		for(int i = points.size()/2; i<points.size(); i++){
+			left.add(points.get(i));
+		}
+		right = maximalPoints(right);
+		
+		//:TODO fix maximal check logic
+		List<point> maximalPoints = new ArrayList<point>();
+		int max;
+		if(right.size()>left.size())
+			max = right.size();
+		else
+			max = left.size();
+		for(int i=0; i<max; i++){
+			
+		}
+		
+		return maximalPoints;
+	}
+}
